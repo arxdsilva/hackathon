02e93520a59898079855564119c0ed10cc20fa3a
Arthur Silva <arxdsilva@gmail.com>
2025-12-26 12:08:45 -0700

    Redirect logged-in users to the hackathons page from the home page

--- actions/home.go
+++ actions/home.go
@@ -3,12 +3,18 @@ package actions
 import (
 	"net/http"
 
+	"hackathon/models"
+
 	"github.com/gobuffalo/buffalo"
 )
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
+	// If user is logged in, redirect to hackathons page
+	if _, ok := c.Value("current_user").(models.User); ok {
+		return c.Redirect(http.StatusFound, "/hackathons")
+	}
 	return c.Render(http.StatusOK, r.HTML("home/index.plush.html"))
 }
 
